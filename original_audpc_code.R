8e93bc018049c80cf48d5d695dc5aced0e32112c
Zachary Foster <zacharyfoster1989@gmail.com>
2016-07-11 22:44:53 -0700

    renamed files

--- original_audpc_code.R
+++ original_audpc_code.R
@@ -1,3 +1,4 @@
+# Load packages
 library('knitr')     # Used to make tables 
 library('dplyr')     # Used for data.frame manipulations
 library('tidyr')     # Used for data.frame manipulations
@@ -7,7 +8,8 @@ library('magrittr')  # Adds %>% operator for chaining commands
 
 
 # Parse input file
-data <- read.csv("http://www.apsnet.org/edcenter/advanced/topics/EcologyAndEpidemiologyInR/DiseaseProgress/Documents/FungicideExample.csv")
+data <- read.csv("fungicide_data.csv")
+
 
 # Tidy up the data
 long_data <- data %>% 
@@ -16,32 +18,31 @@ long_data <- data %>%
   transform(fungicide = ifelse(fungicide, "treated", "untreated")) %>% # make it sensible
   transform(cultivar = gsub("\\.trt$", "", cultivar, ignore.case = TRUE)) # clean up the variable
 
+
 # Graph the disease progress curves
-g <- ggplot(long_data)
-g <- g + geom_line(aes(x = Julian.Date, y = disease, linetype = cultivar))
-g <- g + facet_wrap(~fungicide)
-g <- g + theme_bw()
-g <- g + labs(x = "Julian Day", y = "Percent Disease", title = "Disease Progress Curve")
-g
+dis_plot <- ggplot(long_data)
+dis_plot <- dis_plot + geom_line(aes(x = Julian.Date, y = disease, linetype = cultivar))
+dis_plot <- dis_plot + facet_wrap(~fungicide)
+dis_plot <- dis_plot + theme_bw()
+dis_plot <- dis_plot + labs(x = "Julian Day", y = "Percent Disease", title = "Disease Progress Curve")
+dis_plot
 
 
 # Calculate AUDPC
-# Here I'm calculating AUDPC on each cultivar per fungicide treatment
 audpc_data <- long_data %>% 
-  group_by(cultivar, fungicide) %>% 
+  group_by(cultivar, fungicide) %>% # do any following calculations on every combination of treatments
   summarize(AUDPC = audpc(disease, Julian.Date)) 
-# Displaying it as a table
 audpc_data %>% 
   spread(fungicide, AUDPC) %>% # create a table with fungicide in columns and cultivar in rows
   arrange(untreated) %>%       # Sort by untreated
-  kable()
+  kable() # display as a table
+
 
 # Creating a barplot for the AUDPC results
 audpc_plot <- ggplot(audpc_data, aes(x = fungicide, y = AUDPC, fill = cultivar))
 audpc_plot <- audpc_plot + geom_bar(stat = "identity", position = "dodge")
 audpc_plot <- audpc_plot + theme_bw()
 audpc_plot <- audpc_plot + labs(title = "Area Under the Disease Progress Curve")
-# for color, uncomment the line below
-# audpc_plot <- audpc_plot + scale_fill_brewer(type = "qual", palette = "Set1")
+# audpc_plot <- audpc_plot + scale_fill_brewer(type = "qual", palette = "Set1") # use color
 audpc_plot <- audpc_plot + scale_fill_grey()
 audpc_plot
